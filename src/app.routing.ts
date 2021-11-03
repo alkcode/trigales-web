2780c66ba61da25fc92a99f696b532f2edcee4ab
alkcode <alek_1604@comunidad.unam.mx>
2021-11-03 06:00:16 -0600

    03-11-21 Alkcode
    Se agregaron las vistas de nosotros y servicios

--- src/app.routing.ts
+++ src/app.routing.ts
@@ -1,11 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { InicioComponent } from './app/components/inicio/inicio/inicio.component';
+import { NosotrosComponent } from './app/components/nosotros/nosotros/nosotros.component';
+import { ServiciosComponent } from './app/components/servicios/servicios/servicios.component';
 
 
 
 const appRoutes: Routes=[
     {path:'', component: InicioComponent},
+    {path:'nosotros', component: NosotrosComponent},
+    {path:'servicios', component: ServiciosComponent},
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { AppComponent } from './app.component';
 import { routing, appRoutingProviders } from '../app.routing';
 import { MenuComponent } from './components/menu/menu.component';
 import { InicioComponent } from './components/inicio/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros/nosotros.component';
+import { ServiciosComponent } from './components/servicios/servicios/servicios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    InicioComponent
+    InicioComponent,
+    NosotrosComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule,
